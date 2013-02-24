1a334992b84f65fde086b871b04029d12ff098be
samuli suortti <samuli.suortti@gmail.com>
2013-02-24 15:16:26 +0200

    more apache

--- modules/apache2/manifests/init.pp
+++ modules/apache2/manifests/init.pp
@@ -13,6 +13,13 @@ class apache2 {
       source => "puppet:///modules/apache2/apache2.conf",
       mode => 644,
       owner => root,
-      group => root
+      group => root;
+    "/etc/apache2/sites-enabled/application.conf":
+      source => "puppet:///modules/apache2/application.conf",
+      owner => root,
+      group => root,
+      notify => Service["apache2"],
+      require => Package["apache2"];
+      
   }
 }
