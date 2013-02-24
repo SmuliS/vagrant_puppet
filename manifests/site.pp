f1667b16ccc5f455f5bb44f7c6aa021bf05e6b8a
samuli suortti <samuli.suortti@gmail.com>
2013-02-24 20:33:31 +0200

    postgres module added

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,4 +1,22 @@
 Exec {
   path => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin/bin"
 }
+
 include apache2
+include postgresql
+
+class { 'postgresql::server':
+    config_hash => {
+        'ip_mask_deny_postgres_user' => '0.0.0.0/32',
+        'ip_mask_allow_all_users' => '0.0.0.0/0',
+        'listen_addresses' => '*',
+        'postgres_password' => 'postgres',
+    },
+}
+
+postgresql::db{ 'application_db':
+  user          => 'application_db',
+  password      => 'application_db',
+  grant         => 'all',
+}
+
